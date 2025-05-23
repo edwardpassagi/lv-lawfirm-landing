c8fa700b4566ab61bafd93c96eaf2a35f75301ea
Edward Passagi <edward.passagi@gmail.com>
2025-05-22 19:05:01 -0500

    Update configuration for custom domain lvlaw.id

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-  basePath: process.env.NODE_ENV === 'production' ? '/lv-lawfirm-landing' : '',
+  basePath: '',
 }
 
 module.exports = nextConfig 
